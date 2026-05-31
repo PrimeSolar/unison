ac08b01e947d8a84d59ebbd5c921b833a8d1d3a7
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-05-31 23:53:43 +0300

    Update the class name

--- scripts/p-1-line-indent-min.js
+++ scripts/p-1-line-indent-min.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementsByTagName("p");for(let n of e)null===n.closest(".text-center")&&(n.innerHTML="&nbsp;&nbsp;"+n.innerHTML)}));
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementsByTagName("p");for(let n of e)null===n.closest(".center")&&(n.innerHTML="&nbsp;&nbsp;"+n.innerHTML)}));
